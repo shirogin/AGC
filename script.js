1e758fe17dc990334eee2df8d85962589f767273
shirogin <42269089+shirogin@users.noreply.github.com>
2020-03-28 01:57:09 +0100

    Smooth bar

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
+});
+$(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta >= 0) $(".navbar").removeClass("Top");
+    else $(".navbar").addClass("Top");
 });
